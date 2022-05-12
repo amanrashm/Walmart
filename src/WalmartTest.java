11f0200b09e1847f61b191de585350add51d5ccb
amanrashm <amanra664@gmail.com>
2022-05-12 22:25:00 +0530

    Initial commit

---
+++ src/WalmartTest.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class WalmartTest {
+
+
+
+    public static void main(String args[]){
+        int a[]={1,2,3,5};
+
+        for(int i=0;i<5;i++){
+
+        }
+
+//        int d[]={910,1120,1130,1200,1900,2000};
+//
+//      //  Arrays.sort(d);
+//
+//
+//
+//
+//
+//
+//        int c=1;
+//        for(int i=0;i<a.length-1;i++){
+//            if(a[i+1]<d[i]){
+//                c++;
+//            }
+//        }
+//        System.out.println(c);
+
+    }
+}
